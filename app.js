2f0a1993a0bdf983131aea5ad422f1b0a4baea48
sevakivanyan <sevakivanyan@gmail.com>
2021-05-02 22:11:58 +0400

    - Added page Html (about us) - Added buttons - Added functions for clicking on buttons

--- app.js
+++ app.js
@@ -6,4 +6,29 @@ function changeName(el) {
     } else {
         el.value = userTwo;
     }
-}
+};
+
+function myFunctionOne() {
+    const x = document.getElementById("myDivTwo");
+    const v = document.getElementById("myDivOne")
+    v.style.display = "none"
+    if (x.style.display != "block") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+};
+
+function myFunctionTwo() {
+    const y = document.getElementById("myDivOne");
+    const z = document.getElementById("myDivTwo")
+    z.style.display = "none"
+    if (y.style.display != "block") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+};
+
+
